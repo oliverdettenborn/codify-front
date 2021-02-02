0a43e79246978942cad3551e2d0eeca8887e4131
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-02 18:48:25 -0300

    feat: persistent user data

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,23 @@ import {
   Home, SignIn, SignUp, Course, Profile,
 } from './pages';
 import Globalstyles from './utils/globalstyles';
+import { UserProvider } from './context/UserContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Reset />
-      <Globalstyles />
-      <Switch>
-        <Route path="/" exact component={SignIn} />
-        <Route path="/cadastro" exact component={SignUp} />
-        <Route path="/home" exact component={Home} />
-        <Route path="/cursos/:id" exact component={Course} />
-        <Route path="/perfil" exact component={Profile} />
-      </Switch>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Reset />
+        <Globalstyles />
+        <Switch>
+          <Route path="/" exact component={SignIn} />
+          <Route path="/cadastro" exact component={SignUp} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/cursos/:id" exact component={Course} />
+          <Route path="/perfil" exact component={Profile} />
+        </Switch>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
