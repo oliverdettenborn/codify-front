2ad7276159167ace6ca2cd5e67b74e942a79631f
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-10 14:27:50 -0300

    remove fragments

--- src/pages/Course/components/Banner.jsx
+++ src/pages/Course/components/Banner.jsx
@@ -5,28 +5,26 @@ import {
 
 export default function Banner({ background, courseName, description }) {
   return (
-    <>
-      <CourseInfoContainer
-        width="100%"
-        height="240px"
-        background={background}
-        padding="50px 0"
-        positionRelative
-      >
-        <ButtonClickToBack
-          top="15px"
-          left="15px"
-          size="50"
-          to="/home"
-        />
-        <PlainText fontSize="2.6rem" fontWeight="bold" margin=" 0 0 15px 0" color={(background === '0,0,0' && '#FFF')}>
-          {courseName}
-        </PlainText>
+    <CourseInfoContainer
+      width="100%"
+      height="240px"
+      background={background}
+      padding="50px 0"
+      positionRelative
+    >
+      <ButtonClickToBack
+        top="15px"
+        left="15px"
+        size="50"
+        to="/home"
+      />
+      <PlainText fontSize="2.6rem" fontWeight="bold" margin=" 0 0 15px 0" color={(background === '0,0,0' && '#FFF')}>
+        {courseName}
+      </PlainText>
 
-        <PlainText fontSize="1.5rem">
-          {description}
-        </PlainText>
-      </CourseInfoContainer>
-    </>
+      <PlainText fontSize="1.5rem">
+        {description}
+      </PlainText>
+    </CourseInfoContainer>
   );
 }
