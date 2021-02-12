65676578f7e817cd653573c6c06a9032b2012d5c
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-12 14:26:22 -0300

    add userHasFinish

--- src/pages/Course/components/TopicItem.jsx
+++ src/pages/Course/components/TopicItem.jsx
@@ -13,7 +13,7 @@ export default function Item({ topic }) {
     <Topic flex="1" textAlign="left">
       <div>
         {
-          (topic.topicUsers.length > 0)
+          (topic.userHasFinished > 0)
             ? <FaCheckCircle color="#76DF93" />
             : <BsCircleFill color="#CFCFCF" />
         }
--- src/pages/Course/index.jsx
+++ src/pages/Course/index.jsx
@@ -42,6 +42,7 @@ export default function Course() {
     });
     setLastTopic(last);
   }
+  console.log(chapters);
 
   useEffect(() => {
     axios
