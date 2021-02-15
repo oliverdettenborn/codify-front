c944ae979f6f0e51f3ab6a9bee1ed7209b616ed9
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-15 17:06:12 -0300

    refactor index studyPage

---
+++ src/pages/StudyArea/components/Activity.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from '../../../components';
+
+import YoutubeVideo from './YoutubeVideo';
+import Exercise from './Exercise';
+import Checkbox from './Checkbox';
+
+export default function Activity(props) {
+  const {
+    activity, refresh, setRefresh, changeToNext, index, totalOfActivities,
+  } = props;
+  return (
+    <Container>
+      {activity.youtubeUrl
+        ? <YoutubeVideo link={activity.youtubeUrl} />
+        : <Exercise description={activity.description} />}
+      <Footer>
+        <Checkbox
+          id={activity.theoryId || activity.exerciseId}
+          userHasFinished={activity.userHasFinished}
+          refresh={refresh}
+          setRefresh={setRefresh}
+          type={index === 0 ? 'theories' : 'exercises'}
+        />
+        <Button
+          onClick={changeToNext}
+          width="25%"
+          height="40px"
+          fontsize="15px"
+          borderRadius="8px"
+        >
+          {
+            (index === (totalOfActivities - 1))
+              ? 'Finalizar o tópico >>'
+              : 'Avançar >>'
+          }
+        </Button>
+      </Footer>
+    </Container>
+  );
+}
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  padding: 30px 25% 0 25%;
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 15px;
+`;
--- src/pages/StudyArea/index.jsx
+++ src/pages/StudyArea/index.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState, useContext } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import {
-  CircleLines, YoutubeVideo, Checkbox, CourseDropdown, Exercise,
+  CircleLines, CourseDropdown, Activity,
 } from './components';
-import { Button } from '../../components';
+import { Error, Loading } from '../../components';
 import UserContext from '../../context/UserContext';
+import CourseContext from '../../context/CourseContext';
 
 export default function StudyArea() {
-  const { topicId } = useParams();
+  const { topicId, courseId } = useParams();
   const { user } = useContext(UserContext);
-  const [data, setData] = useState(false);
-  const [topicType, setTopicType] = useState('theory');
+  const { setLastTopicId } = useContext(CourseContext);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [refresh, setRefresh] = useState(false);
+  const [indexActivity, setIndexActivity] = useState(0);
+  const history = useHistory();
 
   useEffect(() => {
     axios
@@ -21,31 +25,58 @@ export default function StudyArea() {
         { headers: { Authorization: `Bearer ${user.token}` } })
       .then((response) => {
         setData([...response.data.theories, ...response.data.exercises]);
+        setLoading(false);
       });
   }, [refresh]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  function changeToNext() {
+    if (indexActivity < data.length) {
+      setIndexActivity(indexActivity + 1);
+    } else {
+      setLastTopicId(topicId);
+      alert('Tópico finalizado');
+      axios
+        .post(
+          `${process.env.REACT_APP_URL_API}/users/${user.userId}/topics/${topicId}/progress`,
+          null,
+          { headers: { Authorization: `Bearer ${user.token}` } },
+        )
+        .then(() => {
+          history.push(`/cursos/${courseId}`);
+        });
+    }
+  }
+
   return (
-    <>
-      <CourseDropdown />
-      {data && <CircleLines list={data} finished={data} /> }
-      {data && (
-        topicType === 'theory'
-          ? (
-            <YoutubeVideo link={data[0].youtubeUrl}>
-              <Container>
-                <Checkbox theoryId={data[0].theoryId} refresh={refresh} setRefresh={setRefresh} />
-                <Button onClick={() => setTopicType('Exercise')} width="25%" height="30px" fontsize="15px" borderRadius="8px">{'Avançar >>'}</Button>
-              </Container>
-            </YoutubeVideo>
+    <Background>
+      {
+        data.length === 0
+          ? <Error>Esse tópico está indisponível, tente novamente mais tarde.</Error>
+          : (
+            <>
+              <CourseDropdown />
+              <CircleLines list={data} finished={data} />
+              <Activity
+                activity={data[indexActivity]}
+                refresh={refresh}
+                setRefresh={setRefresh}
+                changeToNext={changeToNext}
+                totalOfActivities={data.length}
+                index={indexActivity}
+              />
+            </>
           )
-          : <Exercise description={data[1].description} />
-      )}
-    </>
+      }
+    </Background>
   );
 }
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 48vw;
-  height: 50px;
-  padding: 0px 5px;
+
+const Background = styled.div`
+  background: #3d3d3d;
+  width: 100vw;
+  height: 100vh;
 `;
