2da35203b833b12e7d96a079354bfc583da16a12
oliverdettenborn <oliver.dettenborn@gmail.com>
2021-02-19 19:00:46 -0300

    add react-alert and page reset passwword

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,21 @@ import { Reset } from 'styled-reset';
 
 import {
   BrowserRouter,
-  Route,
-  Switch,
 } from 'react-router-dom';
 
-import {
-  Home,
-  SignIn,
-  SignUp,
-  Course,
-  Profile,
-  ForgetPassword,
-  StudyArea,
-  PasswordReset,
-} from './pages';
 import Globalstyles from './utils/globalstyles';
-import { UserProvider } from './context/UserContext';
-import { CourseProvider } from './context/CourseContext';
+import Routes from './routesApp';
+import Providers from './providers';
 
 function App() {
   return (
-    <UserProvider>
-      <CourseProvider>
-        <BrowserRouter>
-          <Reset />
-          <Globalstyles />
-          <Switch>
-            <Route path="/" exact component={SignIn} />
-            <Route path="/cadastro" exact component={SignUp} />
-            <Route path="/home" exact component={Home} />
-            <Route path="/esqueceu-sua-senha" exact component={ForgetPassword} />
-            <Route path="/redefinir-senha" exact component={PasswordReset} />
-            <Route path="/perfil" exact component={Profile} />
-            <Route path="/cursos/:id" exact component={Course} />
-            <Route path="/estudo/:courseId/topic/:topicId" exact component={StudyArea} />
-          </Switch>
-        </BrowserRouter>
-      </CourseProvider>
-    </UserProvider>
+    <Providers>
+      <BrowserRouter>
+        <Reset />
+        <Globalstyles />
+        <Routes />
+      </BrowserRouter>
+    </Providers>
   );
 }
 
--- src/pages/PasswordReset/index.jsx
+++ src/pages/PasswordReset/index.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import axios from 'axios';
 
 import {
   BoxBackground,
@@ -7,22 +10,70 @@ import {
   Input,
   Button,
   TextLink,
+  Error,
 } from '../../components';
 
 export default function PasswordReset() {
-  const [value, setValue] = useState('');
-  const [loading] = useState(false);
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const alert = useAlert();
+
+  const { token } = useParams();
+
+  function handleResetPassword(e) {
+    e.preventDefault();
+    setLoading(true);
+
+    if (password !== repeatPassword) {
+      setError('As senhas precisam ser iguais');
+      setLoading(false);
+    }
+
+    axios
+      .put(`${process.env.REACT_APP_URL_API}/users/password-reset`, {
+        password,
+        passwordConfirmation: repeatPassword,
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => alert.show('senha redefinida'))
+      .catch((err) => {
+        if (err.response && err.response.status === 401) {
+          alert.show('Não foi possível alterar sua senha');
+        } else if (err.response && err.response.status === 404) {
+          alert.show('As senhas precisam ser iguais');
+        } else {
+          alert.show('Houve um erro desconhecido, tente novamente mais tarde');
+        }
+      })
+      .finally(() => setLoading(false));
+  }
 
   return (
     <BoxBackground>
       <Title />
-      <FormBox>
+      <FormBox onSubmit={handleResetPassword}>
         <Input
-          placeholder="digite seu e-mail"
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-          type="email"
+          placeholder="nova senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
         />
+        <Input
+          placeholder="repita sua senha"
+          onChange={(e) => setRepeatPassword(e.target.value)}
+          value={repeatPassword}
+          type="password"
+        />
+
+        {
+          error && <Error>{error}</Error>
+        }
 
         <Button type="submit" disabledButton={loading}>
           recuperar senha
