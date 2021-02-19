a9f9ed18769701b30a92691d8277c96f9f93724e
Lucas Murakami <lkenjim98@gmail.com>
2021-02-19 17:31:05 -0300

    Page for the user to type email to reset the password

--- src/pages/ForgetPassword/index.jsx
+++ src/pages/ForgetPassword/index.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Building } from '../../components';
+import {
+  BoxBackground,
+  Title,
+  FormBox,
+  Input,
+  Button,
+  TextLink,
+} from '../../components';
 
-export default function SignIn() {
-  return <Building />;
+export default function ForgetPassword() {
+  const [value, setValue] = useState('');
+  const [loading] = useState(false);
+
+  return (
+    <BoxBackground>
+      <Title />
+      <FormBox>
+        <Input
+          placeholder="digite seu e-mail"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          type="email"
+        />
+
+        <Button type="submit" disabledButton={loading}>
+          recuperar senha
+        </Button>
+
+        <TextLink
+          to="/"
+          text="voltar para login"
+        />
+      </FormBox>
+    </BoxBackground>
+  );
 }
