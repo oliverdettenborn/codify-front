47d02b43d1946a1c2e02ccacd18b3119436d234b
gibran <gibran.lamarca@engenharia.ufjf.br>
2021-03-03 17:31:56 -0300

    fix: automatically refresh if more than 80% is watched (without pausing the video)

--- src/pages/StudyArea/components/Theory.jsx
+++ src/pages/StudyArea/components/Theory.jsx
@@ -1,50 +1,29 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import YouTube from 'react-youtube';
+import ReactPlayer from 'react-player/youtube';
 import styled from 'styled-components';
-import getYoutubeID from 'get-youtube-id';
 import Footer from './Footer';
 
 export default function Theory(props) {
   const { theory, handleCheckboxChange, checked } = props;
-  const id = getYoutubeID(`${theory.youtubeUrl}`);
-  const [isPlaying, setIsPlaying] = useState(-1);
-  const [videoDuration, setVideoDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
-  function getYoutubeTime(event) {
-    setVideoDuration(event.target.getDuration());
-  }
-  function getCurrentTime(event) {
-    setCurrentTime(event.target.getCurrentTime());
-    setIsPlaying(event.target.getPlayerState());
-  }
-  useEffect(() => {
-    if (isPlaying === 0) {
-      return handleCheckboxChange();
+  const [totalDuration, setTotalDuration] = useState(0);
+  function checkProgress(event) {
+    if (event.playedSeconds / totalDuration >= 0.8 && !checked) {
+      handleCheckboxChange();
     }
-    if (isPlaying === 2) {
-      const percentageWatched = currentTime / videoDuration;
-      if (percentageWatched >= 0.8 && !checked) {
-        return handleCheckboxChange();
-      }
-    }
-  }, [isPlaying]);
-  useEffect(() => {
-    setIsPlaying(-1);
-    setVideoDuration(0);
-    setCurrentTime(0);
-  }, []);
+  }
   return (
     <Box>
       <Container>
         {
         theory.youtubeUrl
           ? (
-            <YouTube
-              videoId={id}
-              onReady={getYoutubeTime}
-              onStateChange={getCurrentTime}
+            <ReactPlayer
+              url={theory.youtubeUrl}
+              controls
+              onProgress={checkProgress}
+              onDuration={(duration) => setTotalDuration(duration)}
             />
           )
           : 'Teoria ainda não foi cadastrada'
